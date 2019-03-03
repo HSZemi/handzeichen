fc88712df5b2877e43e9a3af9fd6b0633c78fb3d
HSZemi <hszemi@users.noreply.github.com>
2019-03-03 16:49:29 +0100

    Simplify gulp task usage

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Help determine the order in which people talk in an online meeting
 ## Setup
 
 - run `npm install`
+- run `gulp`
 - run `node dist/main.js` 
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+var tsProject = ts.createProject("tsconf.json");
 var paths = {
     pages: ['src/*.html']
 };
-
-gulp.task("default", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
-});
 
 gulp.task("copy-html", function () {
     return gulp.src(paths.pages)
         .pipe(gulp.dest("dist"));
 });
+
+gulp.task("compile", function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", gulp.series("copy-html", "compile"));
